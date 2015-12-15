0a7fd8539bbb8d8e06a1859c4bae8451369b7a75
Aaron Lewis <aaron@wandlabs.com>
2015-12-15 15:46:57 -0800

    Test alert

--- wand/wand.js
+++ wand/wand.js
@@ -7,7 +7,7 @@ function execute(url)
   // Credit to http://stackoverflow.com/a/13176471
   var iframe = document.createElement("IFRAME");
   iframe.style.display = "none";
-  iframe..src = url;
+  iframe.src = url;
   document.documentElement.appendChild(iframe);
   document.documentElement.removeChild(iframe);
   iframe = null;
