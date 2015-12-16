14136f51682ee8bf06826dc17abe2cdb74289c97
Aaron Lewis <aaron@wandlabs.com>
2015-12-16 14:56:37 -0800

    Fancy style

--- wand/wand.js
+++ wand/wand.js
@@ -1,18 +1,26 @@
 /*
     WIP
 */
+(function(obj) {
+    var wand = {};
+    a.enabled = false;
+    obj.wand = wand
+})(window);
+(function(window, document, wand) {
+  function execute(url) 
+  {
+    // Credit to http://stackoverflow.com/a/13176471
+    var iframe = document.createElement("IFRAME");
+    iframe.style.display = "none";
+    iframe.setAttribute('src', url);
+    alert(13)
+    document.documentElement.appendChild(iframe);
+    document.documentElement.removeChild(iframe);
+    iframe = null;
+    alert(20)
+  }
 
-function execute(url) 
-{
-  // Credit to http://stackoverflow.com/a/13176471
-  var iframe = document.createElement("IFRAME");
-  iframe.style.display = "none";
-  iframe.setAttribute('src', url);
-  alert(13)
-  document.documentElement.appendChild(iframe);
-  document.documentElement.removeChild(iframe);
-  iframe = null;
-  alert(20)
-}
+  wand.execute = execute
+})(window, document, wand);
 
-execute("wand-client:blah")
+wand.execute("wand-client-action:blah")
