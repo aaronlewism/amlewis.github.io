11af5876d52f810f075c743e593a70304c3e112b
Aaron Lewis <aaron@wandlabs.com>
2016-02-22 11:57:22 -0800

    Movie page

--- fandango/fandango.js
+++ fandango/fandango.js
@@ -411,14 +411,6 @@ $(document).on("pagebeforeshow", "#movie", function() {
   $("#navbar").hide()
   $("#backButton").show()
   $("#backButtonSpan").show()
-
-  var content = $("#movie").find("#content")
-  content.empty()
-
-  var text = $("<h2></h2>");
-  content.text(_fandango_utils.movie_id)
-
-  content.enhanceWithin()
 })
 
 // Keep proper tab selected
